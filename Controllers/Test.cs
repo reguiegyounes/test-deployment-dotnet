6beff28e0946577dc2ec075bb36bdcd4c0c8c6e0
Younes Reguieg <reguiegyounes@gmail.com>
2022-11-05 09:30:39 +0100

    Add Controller test to get values from appsettings

---
+++ Controllers/Test.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace test_deployment_dotnet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Test : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public Test(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+        }
+        [HttpGet]
+        public string Get() {
+            return _configuration.GetConnectionString("Connection");
+        } 
+    }
+}
